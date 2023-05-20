64f34aa8de4103e191ad97b53ef0cd14bd5276ef
kovacseni <kovacseni@gmail.com>
2023-05-20 12:29:42 +0200

    javítás

--- solutions/countOfWorkingDays.js
+++ solutions/countOfWorkingDays.js
@@ -3,16 +3,17 @@
 function countOfWorkingDays(dateEarlier, dateLater) {
     let earlier = new Date(dateEarlier);
     let later = new Date(dateLater);
+    let count = 0;
     if (later < earlier) {
         throw new Error('The first parameter is earlier date, than second!');
-    }
-    let count = 0;
-    while (earlier <= later) {
-        let actualDay = earlier.getDay();
-        if (actualDay > 0 || actualDay < 6) {
-            count++;
+    } else {
+        while (earlier <= later) {
+            let actualDay = earlier.getDay();
+            if (actualDay > 0 || actualDay < 6) {
+                count++;
+            }
+            earlier.setDay(actualDay + 1);
         }
-        earlier.setDay(actualDay + 1);
     }
     return count;
 }
