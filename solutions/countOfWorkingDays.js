3a5d3b92cfc48bf0adf13e4de485f02a54acb164
kovacseni <kovacseni@gmail.com>
2023-05-20 12:20:18 +0200

    elvileg kész

--- solutions/countOfWorkingDays.js
+++ solutions/countOfWorkingDays.js
@@ -1,1 +1,18 @@
+
+
+function countOfWorkingDays(dateEarlier, dateLater) {
+    if (dateLater < dateEarlier) {
+        throw new Error('The first parameter is earlier date, than second!');
+    }
+    let count = 0;
+    while (dateEarlier <= dateLater) {
+        let actualDay = dateEarlier.getDay();
+        if (actualDay > 0 || actualDay < 6) {
+            count++;
+        }
+        dateEarlier.setDay(actualDay + 1);
+    }
+    return count;
+}
+
 export default countOfWorkingDays;
--- solutions/getSecondsOfThisYearUntilNow.js
+++ solutions/getSecondsOfThisYearUntilNow.js
@@ -1,3 +1,10 @@
 
+function getSecondsOfThisYearUntilNow() {
+    let millis = Date.now();
+    let years = Math.trunc(millis / 1000 / 60 / 60 / 24 / 365);
+    let thisYearMillis = millis - (years * 365 * 24 * 60 * 60 * 1000);
+    return thisYearMillis / 1000;
+}
+
 export default getSecondsOfThisYearUntilNow;
 
